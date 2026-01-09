cdc0200d6628d94f1d48392455f46ef17463a04f
thekishanjangid <kishanjangidofficial@gmail.com>
2026-01-09 17:50:02 +0530

    added more content on pages

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ function App() {
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/privacy" element={<PrivacyPolicy />} />
+      <Route path="/disclosure" element={<AffiliateDisclosure />} />
       <Route path="/terms" element={<Terms />} />
       
       {/* New Content Pages */}
